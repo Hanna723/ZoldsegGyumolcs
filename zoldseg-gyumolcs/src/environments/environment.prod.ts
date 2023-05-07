971b1e20c0ad5c26bd34ea02b008742046c1fae5
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2023-05-07 15:26:18 +0200

    Add environments

---
+++ zoldseg-gyumolcs/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebase: {
+      projectId: 'zoldseggyumolcs-3f792',
+      appId: '1:381269773727:web:e3e5137ad30b280bcc81aa',
+      storageBucket: 'zoldseggyumolcs-3f792.appspot.com',
+      apiKey: 'AIzaSyA0ju4T87n7PQdN07K6QFQdk6UedwX29J0',
+      authDomain: 'zoldseggyumolcs-3f792.firebaseapp.com',
+      messagingSenderId: '381269773727',
+      measurementId: 'G-B46Y8WW9CE',
+    },
+    production: false,
+    hostUrl: 'http://localhost:4200',
+  };
+  
